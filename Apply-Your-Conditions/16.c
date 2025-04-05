69e80cd35f28fb46253beab05c959ec58bddc0b4
Priyanto Debnath <priyantodn71@gmail.com>
2025-04-05 22:41:50 +0600

    Add files via upload

---
+++ Apply-Your-Conditions/16.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    float gpa;
+printf("SSC Exam GPA: ");
+scanf("%f",&gpa);
+if(gpa==5){
+    printf("YES, You can apply for the college");
+}else{
+    printf("NO, You can't apply for the college");
+}
+
+
+}
