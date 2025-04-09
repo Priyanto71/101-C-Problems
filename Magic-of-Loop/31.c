f7e7b1edaea9be27096e0bcfccb67488453ba9ba
Priyanto Debnath <priyantodn71@gmail.com>
2025-04-10 03:50:14 +0600

    Add files via upload

---
+++ Magic-of-Loop/31.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+int i,j;
+for(i=1;i<=7;i++){
+        if(i%2==0){
+            printf("\n");
+        }else{
+    for(j=1;j<=i;j++){
+        printf("*");
+    }
+    printf("\n");
+}
+}
+
+}
